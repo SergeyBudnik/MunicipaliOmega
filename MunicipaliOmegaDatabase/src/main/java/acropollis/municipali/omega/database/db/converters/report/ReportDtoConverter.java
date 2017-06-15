f7a8f13d4c051340b4f6f09675765fea7065ee73
Sergey <serbudnik@gmail.com>
2017-06-16 02:52:08 +0300

    base64 encoding fix

--- MunicipaliOmegaDatabase/src/main/java/acropollis/municipali/omega/database/db/converters/report/ReportDtoConverter.java
+++ MunicipaliOmegaDatabase/src/main/java/acropollis/municipali/omega/database/db/converters/report/ReportDtoConverter.java
@@ -3,12 +3,14 @@ package acropollis.municipali.omega.database.db.converters.report;
 import acropollis.municipali.omega.common.dto.report.Report;
 import acropollis.municipali.omega.database.db.model.report.ReportModel;
 
+import static acropollis.municipali.omega.common.utils.common.EncodingUtils.toBase64;
+
 public class ReportDtoConverter {
     public static ReportModel convert(Report report) {
         ReportModel reportModel = new ReportModel(); {
             reportModel.setId(report.getId());
             reportModel.setUserId(report.getUserId());
-            reportModel.setComment(report.getComment());
+            reportModel.setComment(toBase64(report.getComment()));
             reportModel.setLatitude(report.getLatitude());
             reportModel.setLongitude(report.getLongitude());
         }
