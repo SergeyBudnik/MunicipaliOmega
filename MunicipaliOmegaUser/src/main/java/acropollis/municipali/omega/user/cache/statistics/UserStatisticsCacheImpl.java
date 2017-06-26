435c31fe7004960ada6adde78ce9473360332f02
Sergey <serbudnik@gmail.com>
2017-06-26 20:19:59 +0300

    cache concurrency fix

--- MunicipaliOmegaUser/src/main/java/acropollis/municipali/omega/user/cache/statistics/UserStatisticsCacheImpl.java
+++ MunicipaliOmegaUser/src/main/java/acropollis/municipali/omega/user/cache/statistics/UserStatisticsCacheImpl.java
@@ -5,20 +5,36 @@ import org.springframework.stereotype.Service;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @Service
 public class UserStatisticsCacheImpl implements UserStatisticsCache {
-    private Map<Long, Map<Long, Map<Long, Long>>> statisticsCache = new ConcurrentHashMap<>();
+    private AtomicReference<Map<Long, Map<Long, Map<Long, Long>>>> statisticsCache = new AtomicReference<>(new ConcurrentHashMap<>());
+
+    private ReadWriteLock lock = new ReentrantReadWriteLock();
 
     @Override
     public Map<Long, Long> get(long articleId, long questionId) {
-        return statisticsCache
-                .getOrDefault(articleId, new HashMap<>())
-                .getOrDefault(questionId, new HashMap<>());
+        try {
+            lock.readLock().lock();
+
+            return statisticsCache
+                    .get()
+                    .getOrDefault(articleId, new HashMap<>())
+                    .getOrDefault(questionId, new HashMap<>());
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     @Override
     public void set(Map<Long, Map<Long, Map<Long, Long>>> statistics) {
-        this.statisticsCache = statistics;
+        lock.writeLock().lock();
+
+        this.statisticsCache.set(statistics);
+
+        lock.writeLock().unlock();
     }
 }
