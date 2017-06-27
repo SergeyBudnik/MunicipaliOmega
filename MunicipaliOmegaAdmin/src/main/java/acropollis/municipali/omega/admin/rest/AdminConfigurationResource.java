590ec125efa5cfcbd484231971db8d7be9111022
Sergey <serbudnik@gmail.com>
2017-06-27 23:17:21 +0300

    remove auth from admin configuration rest

--- MunicipaliOmegaAdmin/src/main/java/acropollis/municipali/omega/admin/rest/AdminConfigurationResource.java
+++ MunicipaliOmegaAdmin/src/main/java/acropollis/municipali/omega/admin/rest/AdminConfigurationResource.java
@@ -1,40 +1,27 @@
 package acropollis.municipali.omega.admin.rest;
 
 import acropollis.municipali.omega.admin.rest_service.configuration.AdminConfigurationRestService;
-import acropollis.municipali.omega.admin.service.authentication.AdminAuthenticationService;
 import acropollis.municipali.omega.common.dto.configuration.LanguageConfiguration;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/configuration")
-@Api(tags = "Configuration", description = "PROTECTED")
+@Api(tags = "Configuration", description = " ")
 public class AdminConfigurationResource {
     @Autowired
     private AdminConfigurationRestService adminConfigurationRestService;
-    @Autowired
-    private AdminAuthenticationService adminAuthenticationService;
 
-    @GetMapping("/language")
-    public LanguageConfiguration getInterfaceLanguage(
-            @RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false) String authToken
-    ) {
-        return adminConfigurationRestService.getInterfaceLanguage(
-                adminAuthenticationService.getCustomerInfoOrThrow(authToken)
-        );
+    @GetMapping("/language/interface")
+    public LanguageConfiguration getInterfaceLanguage() {
+        return adminConfigurationRestService.getInterfaceLanguage();
     }
 
     @GetMapping("/language/platform")
-    public LanguageConfiguration getPlatformLanguage(
-            @RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false) String authToken
-    ) {
-        return adminConfigurationRestService.getPlatformLanguage(
-                adminAuthenticationService.getCustomerInfoOrThrow(authToken)
-        );
+    public LanguageConfiguration getPlatformLanguage() {
+        return adminConfigurationRestService.getPlatformLanguage();
     }
 }
--- MunicipaliOmegaAdmin/src/main/java/acropollis/municipali/omega/admin/rest_service/configuration/AdminConfigurationRestService.java
+++ MunicipaliOmegaAdmin/src/main/java/acropollis/municipali/omega/admin/rest_service/configuration/AdminConfigurationRestService.java
@@ -1,9 +1,8 @@
 package acropollis.municipali.omega.admin.rest_service.configuration;
 
 import acropollis.municipali.omega.common.dto.configuration.LanguageConfiguration;
-import acropollis.municipali.omega.common.dto.customer.CustomerInfo;
 
 public interface AdminConfigurationRestService {
-    LanguageConfiguration getInterfaceLanguage(CustomerInfo user);
-    LanguageConfiguration getPlatformLanguage(CustomerInfo user);
+    LanguageConfiguration getInterfaceLanguage();
+    LanguageConfiguration getPlatformLanguage();
 }
