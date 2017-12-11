16e69a397c13db377a5d0761ef20987a88dee271
Sergey <serbudnik@gmail.com>
2017-12-11 20:43:36 +0300

    ssh connections removed

--- MunicipaliOmegaUser/src/main/java/acropollis/municipali/omega/user/connection/UserWebListener.java
+++
@@ -1,9 +0,0 @@
-package acropollis.municipali.omega.user.connection;
-
-import acropollis.municipali.omega.common.connection.CommonWebListener;
-
-import javax.servlet.annotation.WebListener;
-
-@WebListener
-public class UserWebListener extends CommonWebListener {
-}
--- MunicipaliOmegaUserNotification/src/main/java/acropollis/municipali/omega/user_notification/connection/UserNotificationWebListener.java
+++
@@ -1,9 +0,0 @@
-package acropollis.municipali.omega.user_notification.connection;
-
-import acropollis.municipali.omega.common.connection.CommonWebListener;
-
-import javax.servlet.annotation.WebListener;
-
-@WebListener
-public class UserNotificationWebListener extends CommonWebListener {
-}
